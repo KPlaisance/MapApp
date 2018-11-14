fbcad3e58fbef66b5c763531b33d6d75359520c4
Michael Plaisance <MPlaisance@paylocity.com>
2018-11-14 07:59:50 -0700

    markers added to map

--- src/component/Map.js
+++ src/component/Map.js
@@ -1,21 +1,31 @@
 import React, { Component } from "react";
 
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+//import MarkerClusterer from "react-google-maps/lib/components/addons/MarkerClusterer";
 
 const MyMapComponent = withScriptjs(
-    withGoogleMap((props) => 
+    withGoogleMap(props => (
   <GoogleMap
     defaultZoom={8}
+    zoom={props.zoom}
     defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    center={props.center}
   >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
-  </GoogleMap>
+    {props.markers && 
+    props.markers
+        .filter(marker => marker.isVisible)
+        .map((marker,idx) => (
+            <Marker key={idx} position={{ lat: marker.lat, lng: marker.lng }} />
+        ))}
+    </GoogleMap>
 ))
+);
 
 export default class Map extends Component {
     render() {
         return (
         <MyMapComponent
+            {...this.props}
             isMarkerShown
             googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDmFcsG0gb6UwbpNJhKQULMeKc3tASlwQw"
             loadingElement={<div style={{ height: `100%` }} />}
